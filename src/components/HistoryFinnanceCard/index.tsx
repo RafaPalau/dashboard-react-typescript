4bcc4ecddc4af1859185915394fbd4bf5e03dae8
RafaPalau <rafaeldonizetip@gmail.com>
2021-03-14 00:00:02 -0300

    Created list styles

---
+++ src/components/HistoryFinnanceCard/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Container, Tag } from "./styles";
+
+interface IHistoryFinnanceCard {
+  tagColor: string;
+  title: string;
+  subtitle: string;
+  amount: string;
+}
+
+const HistoryFinnanceCard: React.FC<IHistoryFinnanceCard> = ({
+    tagColor,
+  title,
+  subtitle,
+  amount,
+}) => {
+  return (
+    <Container >
+      <Tag color={tagColor} />
+      <div>
+        <span>{title}</span>
+        <small>{subtitle}</small>
+      </div>
+      <h3>{amount}</h3>
+    </Container>
+  );
+};
+
+export default HistoryFinnanceCard;
